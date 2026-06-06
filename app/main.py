1d13780613bb3c6f6962f54c3ab522476b680c6c
Nurfadillah Umar <itsennuu@gmail.com>
2026-06-06 22:43:18 +0800

    feat: add prometheus

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.v1.api import api_router
 from app.core.config import settings
@@ -15,3 +16,5 @@ app.add_middleware(
 )
 
 app.include_router(api_router, prefix="/api/v1")
+
+Instrumentator().instrument(app).expose(app)
